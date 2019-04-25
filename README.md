575586c411cb756896f51a61d99f2e7e18ec6424
Anthonyme <46856315+Anthonyme0328@users.noreply.github.com>
2019-04-24 21:29:18 -0700

    finished

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # scraperAME
+
+**what is it**
+
+this is a scraper that pulls from the NY times webiste to give you the latest articles
+
+you can save them leave notes, all you need to do it click the button
+
+**tech**
+
+  - handlebars
+  - mongoDB
+  - custom routes and api
+  - css
+  - node
+  - express
+  - jquery
+  - javascript
+
+** future dev**
+
+more styling to the pages
+
+use a different library other than bootstrap
---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var expressHandlebars = require('express-handlebars');
+var bodyParser = require('body-Parser');
+var mongoose = require('mongoose');
+
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+
+var router= express.Router();
+
+require('./config/routes')(router);
+
+app.use(express.static(__dirname + '/public'));
+
+app.engine('handlebars', expressHandlebars({
+  defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars')
+
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(router);
+
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
+
+app.listen(PORT, function(){
+  console.log('listening on port:' + PORT)
+});
